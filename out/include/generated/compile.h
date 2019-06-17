c611cfff34c49e3f506955500a0bfab7a83ae1d8
EmreCan <dphenix4570@gmail.com>
2019-06-18 01:39:31 +0300

    vestel v6(samos) %60 complate kernel

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#0 SMP PREEMPT Tue Jun 18 00:58:11 +03 2019"
+#define LINUX_COMPILE_BY "romman"
+#define LINUX_COMPILE_HOST "romman"
+#define LINUX_COMPILER "gcc version 4.9.4 20160401 (optimized) (UBERTC-4.9.4) "
---
+++ out/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.18.71-perf-g2a24ad90595f-dirty"
